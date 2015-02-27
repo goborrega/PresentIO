5479381f3543f366881563b5c12e8e61a37f3c31
Gonçalo Borrêga <goncalo.borrega@gmail.com>
2015-02-27 21:48:14 +0000

    Initial Commit

---
+++ PresentIO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PresentIO
+//
+//  Created by Gonçalo Borrêga on 27/02/15.
+//  Copyright (c) 2015 Borrega. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
